fa7adf8c419682c463cffa2848b699490a1903c8
ingvarorn87 <ingvarorn87@gmail.com>
2019-12-11 22:47:19 +0100

    Angular Fire module added

---
+++ src/app/firebase.ts
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyDl9ifd0NdcJFV73r0_ZI4NvH6rlgwJyjc",
+    authDomain: "iotmotionsensor-e737c.firebaseapp.com",
+    databaseURL: "https://iotmotionsensor-e737c.firebaseio.com",
+    projectId: "iotmotionsensor-e737c",
+    storageBucket: "iotmotionsensor-e737c.appspot.com",
+    messagingSenderId: "339756903865",
+    appId: "1:339756903865:web:a9a60863d31aa4c49e1a68",
+    measurementId: "G-64BN25JNFX"
+  }
+
+  export default config

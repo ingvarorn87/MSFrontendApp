f78c4fa9320625c3b565219165b4b10181863825
ingvarorn87 <ingvarorn87@gmail.com>
2019-12-13 20:12:55 +0100

    upload html page fixed

--- src/app/uploader/uploader.page.ts
+++ src/app/uploader/uploader.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UserService } from '../user.service';
+import { firestore } from 'firebase/app';
 
 @Component({
   selector: 'app-uploader',
@@ -10,10 +13,25 @@ export class UploaderPage implements OnInit {
 
 
   imageURL: string
+  desc: string
 
-  constructor(public http: HttpClient) { }
+  constructor(
+    public http: HttpClient,
+    public afstore: AngularFirestore,
+    public user: UserService) { }
 
   ngOnInit() {
+  }
+  createPost(){
+    const image = this.imageURL
+    const desc = this.desc
+
+    this.afstore.doc(`users/${this.user.getUID()}`).update({
+      posts: firestore.FieldValue.arrayUnion({
+        image,
+        desc
+      })
+    })
   }
 
     fileChanged(event) {
